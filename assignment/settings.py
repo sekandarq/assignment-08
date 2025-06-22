09884bd01d0e219523ff8a100ffc615253ef550c
Mohamad Aliff Iskandar <aliff.iskandr02@gmail.com>
2025-06-22 21:24:40 +0900

    Assignment 12 Completed

--- assignment/settings.py
+++ assignment/settings.py
@@ -124,6 +124,11 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files (user-uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- blog/views.py
+++ blog/views.py
@@ -38,3 +38,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def javascript_test(request):
+    return render(request, 'blog/javascript_test.html')   
